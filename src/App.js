2bcafa28823bfbe8b5ac298358cf15f267a518e2
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2023-01-26 15:09:24 +0300

    test3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-  const [name,setName] = useState("Mehmet");
-  const [age,setAge] = useState(29);
-  const [friends,setFriends] = useState(["Ayşe","Fatma"]);
-  const[address,setAddress] = useState({title:"Istanbul",zip:34400});
-
+  const [number,setNumber] = useState(0);
+  const [name,setName] = useState("Mert")
+  useEffect(()=>{
+     console.log("güncellendi")
+  },[number,name]);
 
   return (
-    <div className='App'>
-      <h1> {name}</h1>
-      <h2>{age}</h2>
-      <button onClick={()=> setName("Hariye")}>Change name</button>
-      <button onClick={()=> setAge(25)}>Change age</button>
-      <hr />
-      <br></br>
-    <h2>Friends</h2>
-    {friends.map((friend,index)=>(
-      <div key={index}>{friend}</div>
-    ))}
-    <button onClick={()=> setFriends([...friends,"Hayriye"])}>add new friend</button>
-    <hr />
-      <br></br>
-
-      <h3>Adress</h3>
-       <div>{address.title} {address.zip}</div>
-      
-      <button onClick={()=>setAddress({title:"Ankara",zip:6555})}>Change Adress</button>
-      
+    <div>
+     <h1>{number}</h1>
+      <h2>{name}</h2>
+     <button onClick={()=>setNumber(number+1)}>Click</button>
     </div>
   )
 }
